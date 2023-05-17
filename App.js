7ca425f9873fc7d7f0089a2281380074b941d194
Pierre Lepagnol <pierre.lepagnol@lisn.upsaclay.fr>
2023-05-18 01:44:13 +0200

    adding code useful

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { torch } from 'react-native-pytorch-core';
 
 export default function App() {
+  const [tensor, _setTensor] = useState(torch.rand([2, 3]));
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{`Random tensor of shape ${tensor.shape} with data ${tensor.data()}`}</Text>
       <StatusBar style="auto" />
     </View>
   );
